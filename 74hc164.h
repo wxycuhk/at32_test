fee87f4ccb7d820a36b9624aad441de5f6bb4ece
WANG Xingyu <93190002+wxycuhk@users.noreply.github.com>
2024-09-24 14:40:01 +0800

    Update 74hc164.h

--- 74hc164.h
+++ 74hc164.h
@@ -1,2 +1,16 @@
 #ifndef _74HC164_H
 #define _74HC164_H
+
+#include "sys.h"
+#include "at32f423.h"
+
+# define HC164_CLK_HIGH  PCout(10) = 1
+# define HC164_CLK_LOW   PCout(10) = 0
+
+# define HC164_CLR  PBout(2)
+# define HC164_DAT  PCout(12)
+
+void hc164_init(void);
+void ShiftOut(uint8_t data);
+
+#endif
